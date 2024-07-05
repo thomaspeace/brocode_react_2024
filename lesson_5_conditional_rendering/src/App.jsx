67d088228c7913c6228498bb2f32ffbca054e16a
Thomas Peace <thomasjpeace29@gmail.com>
2024-07-05 21:34:46 +0100

    add lesson 5

--- lesson_5_conditional_rendering/src/App.jsx
+++ lesson_5_conditional_rendering/src/App.jsx
@@ -1,11 +1,13 @@
 // conditional rendering = allows us to control what gets rendered based on certain conditons (show, hide or change)
 
+import UserGreeting from "./UserGreeting"
+
 function App() {
 
 
   return (
     <>
-
+      <UserGreeting isLoggedIn={true} username="Tom"/>
     </>
   )
 }
